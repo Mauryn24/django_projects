c8fa91e0e1ddc7a262835e88a425ef23508409e0
Mauryn24 <maurynnyakio19@gmail.com>
2024-04-27 13:50:22 +0300

    python_hackathon

---
+++ python_hackathon2/python_hackathon2/views.py
@@ -0,0 +1,19 @@
+import requests
+from django.shortcuts import render
+
+
+
+def home(request):
+  # USING APIS => Example 1
+  response = requests.get('https://api.github.com/events')
+  data = response.json()
+  result = data[0]["repo"]
+
+  # Example 2
+  reponse2 = requests.get('https://dog.ceo/api/breeds/image/random')
+  data2 = reponse2.json()
+  result2 = data2["message"]
+
+
+  
+  return render(request, 'home.html', {'result': result, 'result2': result2})
