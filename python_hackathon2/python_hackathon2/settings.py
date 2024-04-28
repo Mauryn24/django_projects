aad603095cf27d9cd14093639422d5dfeab53da0
Mauryn24 <maurynnyakio19@gmail.com>
2024-04-28 15:47:28 +0300

    hackathon

--- python_hackathon2/python_hackathon2/settings.py
+++ python_hackathon2/python_hackathon2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'python_hackathon2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# register css
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- python_hackathon2/python_hackathon2/views.py
+++ python_hackathon2/python_hackathon2/views.py
@@ -14,6 +14,25 @@ def home(request):
   data2 = reponse2.json()
   result2 = data2["message"]
 
+  # Third Api
+  response3 = requests.get(' https://emojihub.yurace.pro/api/random')
+  data3 = response3.json()
+  result3 = data3['htmlCode']
+
+  # Fourth Api
+  # response4 = requests.get('https://coffee.alexflipnote.dev/random')
+  # data4 = response4.json()
+  # result4 = data4['file']
+
+  try:
+    response4 = requests.get('https://emojihub.yurace.pro/api/random')
+    response4.raise_for_status()  # Raise an exception for HTTP errors
+    data4 = response.json()
+    result4 = data.get('emoji', 'No emoji found')  # Get the emoji from the response data
+  except requests.RequestException as e:
+    result4 = f'Error: {e}'  # Handle request exceptions
+  except ValueError as e:
+      result4 = f'Error decoding JSON: {e}'  # Handle JSON decode errors
 
   
-  return render(request, 'home.html', {'result': result, 'result2': result2})
+  return render(request, 'home.html', {'result': result, 'result2': result2, 'result3': result3, 'result4': result4})
