febafeb891618e4fc6344cb4eafd313e83ba0edd
Mauryn24 <maurynnyakio19@gmail.com>
2024-06-12 00:37:05 +0300

    news app

--- newsapp/news_api/views.py
+++ newsapp/news_api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import requests
+API_KEY = '0a47c0fd4f15448481f3fb46f8eecae3'
 
 # Create your views here.
+def home(request):
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'news_api/home.html', context)
